a85c046c03376123370856842540be05dfa1b02a
amyangfei <amyangfei@gmail.com>
2015-03-17 10:51:55 +0800

    Use volume shared directory for storing app log/data and db data.

--- docker/docker_build.sh
+++ docker/docker_build.sh
@@ -2,6 +2,16 @@
 
 scriptdir=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
+app_dir=/var/www/yagra
+app_log_dir=/var/log/yagra
+upload_path=/var/www/yagra/upload
+mysql_data_path=/var/lib/mysql
+
+# used for host volume mapping
+host_log_path="${scriptdir}/../log"
+host_upload_path="${scriptdir}/../upload"
+host_mysql_data_path="${scriptdir}/../data"
+
 clean_old_containers_and_images() {
     sudo docker stop yagra-apache yagra-mysql
     sudo docker rm yagra-apache yagra-mysql
@@ -21,12 +31,18 @@ build_images() {
 }
 
 
+# TODO: docker volume has unsolved problem as non-root user can't write to shared
+# volumes.
+# see: https://github.com/boot2docker/boot2docker/issues/581
+#      https://github.com/docker/docker/issues/7198
 start_containers() {
     sudo docker run --name "yagra-mysql" -h "yagra-mysql" -d -p 3006:3306 \
         -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=yagra \
-        -e MYSQL_PASSWORD=yagra -e MYSQL_DATABASE=yagra yagra/mysql
+        -e MYSQL_PASSWORD=yagra -e MYSQL_DATABASE=yagra \
+        --volumes-from yagra-dbdata yagra/mysql
 
     sudo docker run --name "yagra-apache" -h "yagra-apache" -d -p 8001:80 \
+        --volumes-from yagra-appdata \
         --link yagra-mysql:yagra-mysql yagra/apache
 
     # wait for mysql start up
@@ -34,7 +50,6 @@ start_containers() {
         -h "yagra-db_scheme_loader" -e MYSQL_USER=yagra \
         -e MYSQL_PASSWORD=yagra -e MYSQL_DATABASE=yagra \
         --link yagra-mysql:yagra-mysql yagra/db_scheme_loader
-
 }
 
 clean_old_containers_and_images $*
